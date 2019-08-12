6a8e49acd8a5c711e960fa8e53002934a30dd83f
jason.stone <jason.stone@salesforce.com>
2019-08-11 20:18:50 -0700

    cleanup

--- force-app/main/default/aura/POC_ChannelSecureSsoImageCheck/POC_ChannelSecureSsoImageCheckController.js
+++ force-app/main/default/aura/POC_ChannelSecureSsoImageCheck/POC_ChannelSecureSsoImageCheckController.js
@@ -1,12 +1,24 @@
 ({
     image_load : function(component, event, helper) {
+
         var img = new Image();
+        
         img.onload = function(){
-            alert('Image loaded successfully');
+            component.set("v.isInit", true);
         }
+        
         img.onerror = function(){
-            alert('Failure to load image');
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                mode: 'sticky',
+                title: 'DBS Error',
+                message: 'DBS is unavailable',
+                type: 'error',
+            });
+            toastEvent.fire();
         }
+        
         img.src = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png';
+        
     }
 })
